0581769542984290b60798e264f2ece2e82765e6
Justin Randell <justin.randell@gmail.com>
2011-12-29 21:09:41 +1100

    #1192450 by jherencia: fix permissions for nodejs watchdog messages

--- nodejs_watchdog/nodejs.watchdog.js
+++ nodejs_watchdog/nodejs.watchdog.js
@@ -1,8 +1,9 @@
 
 (function ($) {
 
-Drupal.behaviors.nodejsWatchdog = {
-  attach: function () {
+Drupal.Nodejs.callbacks.nodejsWatchdog = {
+  callback: function (message) {
+    Drupal.nodejs_ajax.runCommands(message);
     $("#admin-dblog tr:even").removeClass('odd').addClass('even');
     $("#admin-dblog tr:odd").removeClass('even').addClass('odd');
   }
